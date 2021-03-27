4895ad48f7e7d967b56b0089d56631de42ff8f6f
chegashi <mochegri@student.1337.ma>
2021-03-27 18:42:53 +0100

    add modulle0 ex04

---
+++ module00/Exercise05__The_right_format/kata00.py
@@ -0,0 +1,2 @@
+t = (19,42,21)
+print('the ', len(t), ' number are:')
---
+++ module00/Exercise05__The_right_format/kata01.py
@@ -0,0 +1,8 @@
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+    }
+
+for key , value in languages:
+    print(key, 'was created by', value)
