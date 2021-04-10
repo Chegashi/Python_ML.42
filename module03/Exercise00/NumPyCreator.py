147e3b66779ac83f0527fb594893faf59e5c74ac
chegashi <mochegri@student.1337.ma>
2021-04-10 19:28:40 +0100

    init module 03

--- module03/Exercise00/NumPyCreator.py
+++ module03/Exercise00/NumPyCreator.py
@@ -1,8 +1,8 @@
-import  numpy as np
+import numpy as np
 import random
 
 class NumPyCreator:
-    def __init():
+    def __init(self):
         pass
 
     def from_list(self, lst):
@@ -32,16 +32,16 @@ class NumPyCreator:
 
 npc = NumPyCreator()
 
-# print(npc.from_list([[1,2,3],[6,3,4]]))
+print(npc.from_list([[1,2,3],[6,3,4]]))
 
-# print(npc.from_tuple(("a", "b", "c")))
+print(npc.from_tuple(("a", "b", "c")))
 
-# print(npc.from_iterable(range(5)))
+print(npc.from_iterable(range(5)))
 
-# shape=(3,5)
+shape=(3,5)
 
-# print(npc.from_shape(shape))
+print(npc.from_shape(shape))
 
-# print(npc.random(shape))
+print(npc.random(shape))
 
 print(npc.identity(4))
--- module03/Exercise01/ImageProcessor.py
+++ module03/Exercise01/ImageProcessor.py
@@ -1,9 +1,17 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 class ImageProcessor:
     def __init(self):
         pass
 
     def load(self, path):
-        pass
+        return mpimg.imread(path)
 
     def Matplotlib(self, arr):
         pass
+
+
+imp = ImageProcessor()
+arr = imp.load('42ai_logo.png')
+print(arr)
