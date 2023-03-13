89a696e240533cf2993628c9f6d5b1886a5f28f2
chegashi <mochegri@student.1337.ma>
2023-03-14 00:57:56 +0100

    add 05

--- Python_Module_04/ex05/HowManyMedalsByCountry.py
+++ Python_Module_04/ex05/HowManyMedalsByCountry.py
@@ -1,12 +1,19 @@
 import numpy as np
+import pandas as pd
 
 def how_many_medals_by_country(data, country_name):
-    dic = {}
+    team_sports = ['Basketball', 'Football',  'Tug-Of-War', 'Badminton',
+                   'Sailing', 'Handball', 'Water Polo', 'Hockey', 'Rowing',
+                   'Bobsleigh', 'Softball', 'Volleyball',
+                   'Synchronized Swimming', 'Baseball', 'Rugby Sevens', 'Rugby',
+                   'Lacrosse', 'Polo']
     data = data.loc[data["Team"] == country_name]
+    data = data[(data['Team'] == country_name) & (~data['Sport'].isin(team_sports))]
+    dictionary = {}
     years = np.unique(data["Year"].array.to_numpy())
     for year in years:
         Gold = len(data.loc[(data["Year"] == year) & (data["Medal"] == "Gold")])
         Bronze = len(data.loc[(data["Year"] == year) & (data["Medal"] == "Bronze")])
         Silver = len(data.loc[(data["Year"] == year) & (data["Medal"] == "Silver")])
-        dic[year] = {'G': Gold, 'S': Silver, 'B': Bronze}
-    return dic
+        dictionary[year] = {'G': Gold, 'S': Silver, 'B': Bronze}
+    print(dictionary)
