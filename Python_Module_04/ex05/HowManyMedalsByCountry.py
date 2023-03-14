58cf5ef9b2b09cb0745e1ee81be8c8107e6f8bc8
chegashi <mochegri@student.1337.ma>
2023-03-14 03:06:50 +0100

    fix  05

--- Python_Module_04/ex05/HowManyMedalsByCountry.py
+++ Python_Module_04/ex05/HowManyMedalsByCountry.py
@@ -8,12 +8,17 @@ def how_many_medals_by_country(data, country_name):
                    'Synchronized Swimming', 'Baseball', 'Rugby Sevens', 'Rugby',
                    'Lacrosse', 'Polo']
     data = data.loc[data["Team"] == country_name]
-    data = data[(data['Team'] == country_name) & (~data['Sport'].isin(team_sports))]
+    data1 = data[(data['Team'] == country_name) & (~data['Sport'].isin(team_sports))]
+    data2 = data[data['Sport'].isin(team_sports)].drop_duplicates(subset=["Team","NOC","Games","City","Sport","Event","Medal"])
     dictionary = {}
     years = np.unique(data["Year"].array.to_numpy())
     for year in years:
-        Gold = len(data.loc[(data["Year"] == year) & (data["Medal"] == "Gold")])
-        Bronze = len(data.loc[(data["Year"] == year) & (data["Medal"] == "Bronze")])
-        Silver = len(data.loc[(data["Year"] == year) & (data["Medal"] == "Silver")])
+        Gold = len(data1.loc[(data1["Year"] == year) & (data1["Medal"] == "Gold")])
+        Gold += len(data2.loc[(data["Year"] == year) & (data2["Medal"] == "Gold")])
+        Bronze = len(data1.loc[(data["Year"] == year) & (data1["Medal"] == "Bronze")])
+        Bronze += len(data2.loc[(data["Year"] == year) & (data2["Medal"] == "Bronze")])
+        Silver = len(data1.loc[(data["Year"] == year) & (data1["Medal"] == "Silver")])
+        Silver += len(data2.loc[(data["Year"] == year) & (data2["Medal"] == "Silver")])
         dictionary[year] = {'G': Gold, 'S': Silver, 'B': Bronze}
     print(dictionary)
+    return(dictionary)
